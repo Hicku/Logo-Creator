af0d0097e0a7a202c7352d950c740654d728f2ed
Quas <aaron.hickman201@gmail.com>
2023-04-11 11:42:49 +0100

    svg.js

---
+++ assets/index.js
@@ -0,0 +1,15 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+
+const questions = [
+    {
+      type: 'input',
+      name: 'Colour',
+      message: "What will be the logo's base colour?",
+    },
+    {
+
+    },
+  
+  ];
