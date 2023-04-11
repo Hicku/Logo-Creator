275a578940915cc54f59a868574c7e3665cc7e86
Quas <aaron.hickman201@gmail.com>
2023-04-11 12:18:06 +0100

    questions

--- assets/index.js
+++ assets/index.js
@@ -3,13 +3,21 @@ const fs = require('fs');
 
 
 const questions = [
-    {
-      type: 'input',
-      name: 'Colour',
-      message: "What will be the logo's base colour?",
-    },
-    {
-
-    },
-  
-  ];
+        {
+          type: "list",
+          name: "color",
+          message: "What color do you want for your logo?",
+          choices: ["red", "green", "blue", "yellow", "orange"]
+        },
+        { 
+          type: "list",
+          name: "shape", 
+          message: "What shape do you want your logo to be?",
+          choices: ["square", "circle", "triangle", "rectangle", "star"]  
+        },
+        {
+         type: "input",
+         name: "text",
+         message: "What text do you want in the logo?"           
+        },
+]
