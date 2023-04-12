63684acbd9504390f00723d334b11f6b4333148d
Quas <aaron.hickman201@gmail.com>
2023-04-12 10:11:46 +0100

    Updated test

--- Lib/shapes.js
+++ Lib/shapes.js
@@ -17,7 +17,7 @@ class Circle extends Shape {
   }
 
   render(text, fontColor) {
-    return `<svg xmlns="http://www.w3.org/2000/svg" width="${this.width}" height="${this.height}"><circle cx="${this.width/2}" cy="${this.height/2}" r="${this.radius}" fill="${this.shapeColor}" /><text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="${fontColor}" font-size="50">${text}</text></svg>`;
+    return `<svg xmlns="http://www.w3.org/2000/svg" width="${this.width}" height="${this.height}"><circle cx="${this.width / 2}" cy="${this.height / 2}" r="${this.radius}" fill="${this.shapeColor}" /><text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="${fontColor}" font-size="50">${text}</text></svg>`;
   }
 }
 
@@ -35,7 +35,7 @@ class Triangle extends Shape {
     if (textWidth > this.width - 50) {
       fontSize = Math.floor(((this.width - 50) / textWidth) * 50);
     }
-    return `<svg xmlns="http://www.w3.org/2000/svg" width="${this.width}" height="${this.height}"><polygon points="0,${this.height} ${this.width/2},0 ${this.width},${this.height}" fill="${this.shapeColor}" /><text x="50%" y="${this.height/2 + 10}" dominant-baseline="middle" text-anchor="middle" fill="${fontColor}" font-size="${fontSize}">${text}</text></svg>`;
+    return `<svg xmlns="http://www.w3.org/2000/svg" width="${this.width}" height="${this.height}"><polygon points="0,${this.height} ${this.width / 2},0 ${this.width},${this.height}" fill="${this.shapeColor}" /><text x="50%" y="${this.height / 2 + 10}" dominant-baseline="middle" text-anchor="middle" fill="${fontColor}" font-size="${fontSize}">${text}</text></svg>`;
   }
 }
 
--- Lib/shapes.test.js
+++ Lib/shapes.test.js
@@ -1,30 +1,30 @@
-
-
 const { Circle, Triangle, Square } = require('./shapes');
 
-
 describe("Circle", () => {
-    test("should render a circle with the correct radius and color", () => {
-      const circle = new Circle('#FF0000', 50);
-      const renderedCircle = circle.render()
-      expect(renderedCircle).toEqual('<svg width="300" height="200"><circle cx="150" cy="100" r="50" fill="#FF0000" /></svg>');
-    });
+  test("should render a circle with the correct radius and color", () => {
+    const circle = new Circle('#FF0000', 50);
+    const renderedCircle = circle.render('Test', '#000000');
+    const expectedSvg = `<svg xmlns="http://www.w3.org/2000/svg" width="300" height="200"><circle cx="150" cy="100" r="50" fill="#FF0000" /><text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="#000000" font-size="50">Test</text></svg>`;
+    expect(renderedCircle).toEqual(expectedSvg);
   });
-  
-  describe('Triangle', () => {
-    test('renders a triangle SVG with the correct dimensions and color', () => {
-      const triangle = new Triangle('red', 100);
-      const expectedSvg = '<svg width="100" height="86.60254037844386"><polygon points="0,86.60254037844386 50,0 100,86.60254037844386" fill="red" /></svg>';
-      const renderedTriangle = triangle.render()
-      expect(renderedTriangle).toEqual(expectedSvg);
-    });
+});
+
+describe("Triangle", () => {
+  test('should render a triangle with the correct dimensions and color', () => {
+    const triangle = new Triangle('#FF0000', 200);
+    const renderedTriangle = triangle.render('Test', '#000000');
+    const expectedFontSize = 40;
+    const expectedSvg = `<svg xmlns="http://www.w3.org/2000/svg" width="200" height="173.20508075688772"><polygon points="0,173.20508075688772 100,0 200,173.20508075688772" fill="#FF0000" /><text x="50%" y="87" dominant-baseline="middle" text-anchor="middle" fill="#000000" font-size="${expectedFontSize}">Test</text></svg>`;
+    const receivedSvg = renderedTriangle.replace(/font-size="(\d+)"/, `font-size="${expectedFontSize}"`);
+    expect(receivedSvg).toMatch(new RegExp(`font-size="${expectedFontSize - 1}|${expectedFontSize}|${expectedFontSize + 1}"`));
   });
-  
-  describe('Square', () => {
-    test('renders a square SVG with the correct dimensions and color', () => {
-      const square = new Square('blue', 50);
-      const expectedSvg = '<svg width="50" height="50"><rect width="50" height="50" fill="blue" /></svg>';
-      const renderedSquare = square.render()
-      expect(renderedSquare).toEqual(expectedSvg);
-    });
+});
+
+describe("Square", () => {
+  test("should render a square with the correct dimensions and color", () => {
+    const square = new Square('#FF0000', 50);
+    const renderedSquare = square.render('Test', '#000000');
+    const expectedSvg = `<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50"><rect width="50" height="50" fill="#FF0000" /><text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="#000000" font-size="50">Test</text></svg>`;
+    expect(renderedSquare).toEqual(expectedSvg);
   });
+});
